66ad41d1bce86a6d53cf9eb53393c1d4cabf6109
jorgitodf <jspaiva.1977@gmail.com>
2017-04-05 21:44:43 -0300

    first commit

---
+++ src/Application.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+namespace Financas;
+
+
+use Financas\Plugins\PluginInterface;
+
+class Application
+{
+    private $serviceContainer;
+
+    /**
+     * Application constructor.
+     * @param $serviceContainer
+     */
+    public function __construct(ServiceContainerInterface $serviceContainer)
+    {
+        $this->serviceContainer = $serviceContainer;
+    }
+
+    public function service($name) {
+        $this->serviceContainer->get($name);
+    }
+
+    public function addService(string $name, $service): void {
+        if (is_callable($service)) {
+            $this->serviceContainer->addLazy($name, $service);
+        } else {
+            $this->serviceContainer->add($name, $service);
+        }
+    }
+
+    public function plugin(PluginInterface $plugin): void {
+        $plugin->register($this->serviceContainer);
+    }
+
+}
